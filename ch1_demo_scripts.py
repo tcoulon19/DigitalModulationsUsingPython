d8ad1fa0ebf9abaf0c3d002b7a3c0626d627a6b6
Thomas Coulon <tcoulon19@restdnn.ad.hume.vt.edu>
2022-10-20 13:28:54 -0400

    saving

--- ch1_demo_scripts.py
+++ ch1_demo_scripts.py
@@ -61,6 +61,22 @@ def rectangular_pulse_demo():
     plt.savefig('Rect pulse.png') # display the figure
 
 
+def gaussian_pulse_demo():
+
+    from signalgen import gaussian_pulse
+    import matplotlib.pyplot as plt
+
+    fs = 500
+    sigma = .1
+    (t,g) = gaussian_pulse(fs, sigma)
+
+    plt.plot(t,g) # plot using pyplot library from matplotlib package
+    plt.title('Gaussian pulse') # plot title
+    plt.xlabel('Time (s)') # x-axis label
+    plt.ylabel('Amplitude') # y-axis label
+    plt.savefig('Gaussian pulse.png') # display the figure
+
+
 def chirp_demo():
     """
     Generating and plotting a chirp signal
@@ -68,6 +84,13 @@ def chirp_demo():
     import numpy as np
     import matplotlib.pyplot as plt
     from scipy.signal import chirp
+
+    '''
+    f0: starting frequency
+    t1: time at end of T
+    f1: frequency at end of T
+    phi: starting phase
+    '''
 
     fs = 500 # sampling frequency in Hz
     t =np.arange(start = 0, stop = 1,step = 1/fs) #total time base from 0 to 1 second
@@ -80,6 +103,7 @@ def chirp_demo():
     plt.savefig('Chirp signal.png') # display the figure
 
 
+# For this fft example, you have option to use fftshift. Comment/uncomment accordingly.
 def fft_example_1():
 
     from scipy.fftpack import fft, ifft, fftshift
@@ -134,46 +158,56 @@ def fft_example_2():
     phase=30 # desired phase shift of the cosine in degrees
     fs=32*fc # sampling frequency with oversampling factor 32
     t=np.arange(start = 0,stop = 2,step = 1/fs) # 2 seconds duration
+
     phi = phase*np.pi/180; # convert phase shift in degrees in radians
     x=A*np.cos(2*np.pi*fc*t+phi) # time domain signal with phase shift
 
-    fig, (ax1, ax2, ax3, ax4) = plt.subplots(nrows=4, ncols=1)
-    ax1.plot(t,x) # plot time domain representation
-    ax1.set_title(r'$x(t) = 0.5 cos (2 \pi 10 t + \pi/6)$')
-    ax1.set_xlabel('time (t seconds)');ax1.set_ylabel('x(t)')
+    plt.figure(0)
+    plt.plot(t,x) # plot using pyplot library from matplotlib package
+    plt.title('Time domain') # plot title
+    plt.xlabel('t (s)') # x-axis label
+    plt.ylabel('Amplitude') # y-axis label
+    plt.savefig('fft_example_2_im1.png') # display the figure
 
-    N=256 # FFT size
+    # Calculate fft and extract amplitude info
+    N=fs # FFT size
     X = 1/N*fftshift(fft(x,N)) # N-point complex DFT
 
     df=fs/N # frequency resolution
     sampleIndex = np.arange(start = -N//2,stop = N//2) # // for integer division
     f=sampleIndex*df # x-axis index converted to ordered frequencies
 
-    ax2.stem(f,abs(X), use_line_collection=True) # magnitudes vs frequencies
-    ax2.set_xlim(-30, 30)
-    ax2.set_title('Amplitude spectrum')
-    ax2.set_xlabel('f (Hz)');ax2.set_ylabel(r'$ \left| X(k) \right|$')
+    plt.figure(1)
+    plt.stem(f,abs(X)) # plot using pyplot library from matplotlib package
+    plt.title('Amplitude vs Frequency') # plot title
+    plt.xlabel('f (Hz)') # x-axis label
+    plt.ylabel('Amplitude') # y-axis label
+    plt.savefig('fft_example_2_im2.png') # display the figure
 
-    phase=np.arctan2(np.imag(X),np.real(X))*180/np.pi # phase information
-    ax3.plot(f,phase) # phase vs frequencies
-
+    # Extract phase info.
+    # Note: Because arctan is sensitive to small ammounts of noise, zero noise below threshold
     X2=X #store the FFT results in another array
     # detect noise (very small numbers (eps)) and ignore them
     threshold = max(abs(X))/10000; # tolerance threshold
     X2[abs(X)<threshold]=0 # maskout values below the threshold
     phase=np.arctan2(np.imag(X2),np.real(X2))*180/np.pi # phase information
 
-    ax4.stem(f,phase, use_line_collection=True) # phase vs frequencies
-    ax4.set_xlim(-30, 30); ax4.set_title('Phase spectrum')
-    ax4.set_ylabel(r"$\angle$ X[k]");ax4.set_xlabel('f(Hz)')
-    fig.show()
+    plt.figure(2)
+    plt.stem(f,phase) # plot using pyplot library from matplotlib package
+    plt.title('Phase vs Frequency') # plot title
+    plt.xlabel('f (Hz)') # x-axis label
+    plt.ylabel('Phase') # y-axis label
+    plt.savefig('fft_example_2_im3.png') # display the figure
 
     x_recon = N*ifft(ifftshift(X),N) # reconstructed signal
     t = np.arange(start = 0,stop = len(x_recon))/fs # recompute time index
     
-    fig2, ax5 = plt.subplots()
-    ax5.plot(t,np.real(x_recon)) # reconstructed signal
-    ax5.set_title('reconstructed signal')
-    ax1.set_xlabel('time (t seconds)');ax1.set_ylabel('x(t)');
-    fig2.show()
+    plt.figure(3)
+    plt.plot(t,x_recon) # plot using pyplot library from matplotlib package
+    plt.title('Time domain, reconstructed') # plot title
+    plt.xlabel('t (s)') # x-axis label
+    plt.ylabel('Amplitude') # y-axis label
+    plt.savefig('fft_example_2_im4.png') # display the figure
+
+fft_example_2()
 
