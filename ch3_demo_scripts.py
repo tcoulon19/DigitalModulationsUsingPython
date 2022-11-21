88288ca3faf07abe99074e84f9a6dbc1921b565b
Thomas Coulon <tcoulon19@ad.hume.vt.edu>
2022-11-21 16:02:04 -0500

    saving

--- ch3_demo_scripts.py
+++ ch3_demo_scripts.py
@@ -1,20 +1,20 @@
 def modem_demo():
 
     import numpy as np
-    from modem import PSKModem
+    from modem import PSKModem, QAMModem
 
     M = 16 # 16 points in the constellation
-    pskModem = PSKModem(M) # Create a 16-PSK modem object
-    pskModem.plotConstellation() # Plot ideal constellation for this modem
+    qamModem = QAMModem(M) # Create a 16-PSK modem object
+    qamModem.plotConstellation() # Plot ideal constellation for this modem
 
     nSym = 10 # 10 symbols as input to PSK modem
     inputSyms = np.random.randint(low=0, high=M, size=nSym) # Uniform random symbols from 0 to M-1
     print('inputSyms: ' + str(inputSyms))
 
-    modulatedSyms = pskModem.modulate(inputSyms) # Modulate
+    modulatedSyms = qamModem.modulate(inputSyms) # Modulate
     print('modulatedSyms: ' + str(modulatedSyms))
 
-    detectedSyms = pskModem.demodulate(modulatedSyms) # Demodulate
+    detectedSyms = qamModem.demodulate(modulatedSyms) # Demodulate
     print('detectedSyms: ' + str(detectedSyms))
 
 modem_demo()
