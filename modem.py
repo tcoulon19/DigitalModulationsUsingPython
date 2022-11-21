6b1dc65cceb49d16e9f4aedfc17a0969a7f3d051
Thomas Coulon <tcoulon19@ad.hume.vt.edu>
2022-11-21 12:34:01 -0500

    added Modem

--- modem.py
+++ modem.py
@@ -37,6 +37,46 @@ class Modem:
         if self.name.lower() == 'fsk':
             return 0 # FSK is multi-dimenstional, difficult to visualize
 
+        fig, axs = plt.subplots(1,1)
+        axs.plot(np.real(self.constellation),np.imag(self.constellation),'o')
+
+        for i in range(0,self.M):
+            axs.annotate("{0:0{1}b}".format(i,self.M), (np.real(self.constellation[i]), np.imag(self.constellation[i])))
+
+        axs.set_title('Constellaton')
+        axs.set_xlabel('I')
+        axs.set_ylabel('Q')
+        fig.show()
+
+    def modulate(self,inputSymbols):
+
+        '''
+        Modulate a vector of input symbols (numpy array format) using the chosen modem.
+        Input symbols take integer values in the range 0 to M-1.
+        '''
+        if isinstance(inputSymbols, list):
+            inputSymbols = np.array(inputSymbols)
+
+        if not (0 <= inputSymbols.all() <= self.M-1):
+            raise ValueError('inputSymbols values are beyond the range 0 to M-1')
+
+        modulatedVec = self.constellation[inputSymbols]
+        return modulatedVec # Return modulated vector
+
+    def demodulate(self,receivedSyms):
         
+        '''
+        Demodulate a vector of received symbols using the chosen modem.
+        '''
+        if isinstance(receivedSyms,list):
+            receivedSyms = np.array(receivedSyms)
+
+        detectedSyms = self.iqDetector(receivedSyms)
+        return detectedSyms
+
+    def iqDetector(self,receivedSyms):
+        '''
+        Optimum Detector for 2-dim. signals (ex: MQAM,MPSK,MPAM) in IQ plane
+        '''
 
     
