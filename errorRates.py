9e26ed764b3ac4e07d4af20856e045886d97c135
Thomas Coulon <tcoulon19@ad.hume.vt.edu>
2022-12-14 16:12:16 -0500

    saving

--- errorRates.py
+++ errorRates.py
@@ -47,3 +47,35 @@ def psk_awgn(M,gamma_s):
 
 def qam_awgn(M,gamma_s):
     if (M==1) or (np.mod(np.log2(M),2)!=0): # M not an even power of 2
+        raise ValueError('Only square MQAM supported. M must be even power of 2')
+    SERs = 1-(1-(1/sqrt(M))*erfc(sqrt(3/2*gamma_s/(M-1))))**2
+    return SERs
+
+
+def pam_awgn(M,gamma_s):
+    SERs=2*(1-1/M)*.5*erfc(sqrt(3*gamma_s/(M**2-1)))
+    return SERs
+
+
+def integrand(q,gamma_s,M):
+    return (.5*erfc((-q-np.sqrt(2*gamma_s))/np.sqrt(2)))**(M-1)*1/np.sqrt(2*pi)*np.exp(-(q**2)/2)
+
+
+def fsk_awgn(M_val,gamma_s_vals,coherence):
+    SERs = np.zeros(len(gamma_s_vals))
+    if coherence.lower() == 'coherent':
+        for j,gamma_s in enumerate(gamma_s_vals):
+            (y,_) = quad(integrand,-np.inf,np.inf, (gamma_s, M_val))
+            SERs[j] = 1-y
+    elif coherence.lower()=='noncoherent':
+        # Use SymPy - symbolic mathematics for evaluating the SER equations
+        from sympy import symbols, Symbol, Sum, exp, binomial, erfc, integrate, oo, sqrt
+        M,i = symbols('M i',integer=True,positive=True)
+        gamma = Symbol('gamma')
+        s = Sum((-1)**(i+1)/(i+1)*binomial(M-1,i)*exp(-i/(i+1)*gamma),(i,1,M-1))
+        for j,gamma_s in enumerate(gamma_s_vals):
+            # Evaluate the expression with values for M and gamma_s
+            SERs[j] = s.evalf(subs={M:M_val,gamma:gamma_s})
+    else:
+        raise ValueError('For FSK coherence must be \'coherent\' or \'noncoherent\'')
+    return SERs
